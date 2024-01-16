55ea4b91a05d998bf0b5f1439449bffc559aa06f
Adam Haertter <80988984+adamhaertter@users.noreply.github.com>
2024-01-16 17:53:51 -0500

    Rename QueryBuilder -> QueryWrapper

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import requests
-import apiQueryBuilder
+import apiQueryWrapper
 import os
 
 url = "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=1"
-response = requests.get(apiQueryBuilder.getPlayerAchievements(apiQueryBuilder.test_app_id))
+response = requests.get(apiQueryWrapper.getPlayerAchievements(apiQueryWrapper.test_app_id))
 
 if response.status_code == 200:
     json_data = response.json()
